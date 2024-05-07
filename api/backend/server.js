e09b1e00ad3fa87c63d278b440de404c1894c219
Wenkang <166893688+Wenkangy@users.noreply.github.com.>
2024-05-07 01:03:41 -0700

    trying the code

---
+++ api/backend/server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const chats = require("./data");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.options("*", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "GET,PUT,POST,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, Content-Length, X-Requested-With"
+  );
+  res.send(200);
+});
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+  res.send("api running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.json(chats);
+});
+
+app.get("/auth/userName/:userName/pswd/:pswd", (req, res) => {
+  if (verify_login(req.params.userName, req.params.pswd)) {
+    var token = jwt.sign({ username: `${req.params.userName}` }, "secret", {
+      expiresIn: 120,
+    });
+    res.send(token);
+  }
+  res.send(req.params);
+});
+
+app.all("*", (req, res) => {
+  res.send(req.params);
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+function verify_login(userName, pswd) {
+  if (userName == "kufooloo" && pswd == "secret") {
+    return true;
+  }
+  return false;
+}
